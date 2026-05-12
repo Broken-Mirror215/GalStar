7e11275d1adeea2c61614f7e612fc3fa60495877
broken <wangzhezhilu1245@gmail.com>
2026-05-12 20:15:22 +0800

    add authorition

--- go.mod
+++ go.mod
@@ -7,6 +7,4 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	golang.org/x/crypto v0.26.0
 	github.com/gin-contrib/cors v1.7.2
-	
-
 )
---
+++ internal/middleware/auth.go
@@ -0,0 +1,48 @@
+package middleware
+
+import (
+	"Gal-Finder/internal/response"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte("change this secret")
+
+type Claims struct {
+	UserID uint `json:"userid"`
+	jwt.RegisteredClaims
+}
+
+func JWTAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			response.Fail(c, 401, 401, "missing token")
+			c.Abort()
+			return
+		}
+
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			response.Fail(c, 400, 401, "token format error")
+		}
+		token, err := jwt.ParseWithClaims(parts[1], &Claims{}, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+		if err != nil || !token.Valid {
+			response.Fail(c, 400, 401, "invalid token")
+			c.Abort()
+			return
+		}
+		claims, ok := token.Claims.(*Claims)
+		if !ok {
+			response.Fail(c, 400, 401, "invalid token")
+			c.Abort()
+			return
+		}
+		c.Set("userID", claims.UserID)
+		c.Next()
+	}
+}
