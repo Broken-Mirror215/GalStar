52cc7fa2f2f0314db86890e5cf0587c71203f63b
broken <wangzhezhilu1245@gmail.com>
2026-05-14 14:25:00 +0800

    finish ratelimit

---
+++ internal/middleware/ratelimit.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"Gal-Finder/internal/response"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type visitor struct {
+	Count     int
+	ResetTime time.Time
+}
+
+var (
+	visitors = make(map[string]*visitor)
+	mu       sync.Mutex //这看起来像一个异步锁
+)
+
+func Ratelimit(limit int, window time.Duration) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		key := c.ClientIP()
+
+		if UserID, ok := c.Get("user_id").(string); ok {
+			fmt.Printf("user %d", UserID)
+		}
+
+		now := time.Now()
+
+		mu.Lock()
+		v, ok := visitors[key]
+		if !ok || now.After(v.ResetTime) {
+			visitors[key] = &visitor{
+				Count:     1,
+				ResetTime: now.Add(window), //这是什么？？
+			}
+			mu.Unlock()
+			c.Next()
+			return
+		}
+
+		if v.Count >= limit {
+			mu.Unlock()
+			response.Fail(c, 401, 401, "too many requests")
+			c.Abort()
+			return
+		}
+		v.Count++
+		mu.Unlock()
+		c.Next()
+	}
+
+}
